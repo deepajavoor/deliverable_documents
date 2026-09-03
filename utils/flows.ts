e5798e4f24410e1bb2b3289299331ee81d1425a0
deepajavoor <deepajavoor@gmail.com>
2026-09-03 08:25:44 +0530

    Delete utils directory

--- utils/flows.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from '@playwright/test';
-import { LoginPage, InventoryPage } from '../pages/AppPages';
-import { users } from '../test-data/data';
-export async function loginStandard(page: Page) {
-  const login = new LoginPage(page); const inventory = new InventoryPage(page);
-  await login.goto(); await login.login(users.standard.username, users.standard.password); await inventory.expectLoaded();
-}
